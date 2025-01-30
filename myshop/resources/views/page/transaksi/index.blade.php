54a2c2ec0eb646765ba26884ed463a177e412bd7
Arafian01 <arafianw@gmail.com>
2025-01-30 23:20:04 +0700

    finish v1

--- myshop/resources/views/page/transaksi/index.blade.php
+++ myshop/resources/views/page/transaksi/index.blade.php
@@ -3,7 +3,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="text-right p-4">
-                    <form method="GET" action="{{ route('produk.index') }}" class="mb-3 flex items-center gap-2">
+                    <form method="GET" action="{{ route('transaksi.index') }}" class="mb-3 flex items-center gap-2">
                         <input type="text" id="search" name="search" value="{{ request('search') }}"
                             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2.5 w-full max-w-xs"
                             placeholder="Cari barang...">
@@ -15,22 +15,26 @@
                     <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
                         @foreach ($produk as $p)
                             <div class="p-4 bg-white shadow-md rounded-lg w-full mt-4">
-                                <div class="flex flex-col sm:flex-row items-center gap-4 px -4">
+                                <div class="flex flex-col sm:flex-row items-center gap-4 px-4">
                                     <!-- Gambar di atas pada HP, kiri pada layar besar -->
-                                    <img src="{{ $p->image ? asset('storage/' . $p->image) : asset('default.jpg') }}" alt="Gambar"
-                                        class="w-40 h-40 rounded-lg object-cover">
-                                    
+                                    <img src="{{ $p->image ? asset('storage/' . $p->image) : asset('default.jpg') }}"
+                                        alt="Gambar" class="w-40 h-40 rounded-lg object-cover">
+
                                     <!-- Teks di bawah gambar pada HP, di samping pada layar besar -->
                                     <div class="flex-1 text-center sm:text-left">
                                         <h2 class="text-xl font-bold">{{ $p->name }}</h2>
-                                        <p class="text-lg font-semibold text-yellow-600">Rp. {{ number_format($p->harga_jual) }}</p>
+                                        <p class="text-lg font-semibold text-yellow-600">Rp.
+                                            {{ number_format($p->harga_jual) }}</p>
                                         <p class="text-sm text-gray-500">Kategori : {{ $p->kategori->name }}</p>
-                                        <p class="text-sm text-gray-500">Jumlah : {{ $p->jumlah }} {{ $p->satuan }}</p>
+                                        <p class="text-sm text-gray-500">Jumlah : {{ $p->jumlah }}
+                                            {{ $p->satuan }}</p>
                                     </div>
                                 </div>
                                 <!-- Tombol di bawah pada HP, kanan pada layar besar -->
                                 <div class="flex justify-center sm:justify-end p-4">
-                                    <button class="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600 w-full sm:w-auto">Aksi</button>
+                                    <button type="button"
+                                        onclick="addCart('{{ $p->id }}', '{{ $p->name }}', '{{ $p->harga_jual }}')"
+                                        class="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600 w-full sm:w-auto">Tambah</button>
                                 </div>
                             </div>
                         @endforeach
@@ -43,139 +47,57 @@
         </div>
     </div>
 
-    <!-- Edit Modal -->
-    <div class="fixed inset-0 flex items-center justify-center z-50 hidden" id="sourceModalEdit">
-        <div class="fixed inset-0 bg-black opacity-50" onclick="sourceModalClose()"></div>
+    <div class="fixed inset-0 items-center justify-center z-50 hidden" id="sourceModal">
+        <div class="fixed inset-0 bg-black opacity-50"></div>
         <div class="fixed inset-0 flex items-center justify-center">
-            <div class="w-full md:w-1/2 relative bg-white rounded-lg shadow mx-5 max-h-[90vh] overflow-y-auto">
-                <div class="flex items-start justify-between p-4 border-b rounded-t sticky top-0 bg-white z-10">
-                    <h3 class="text-xl font-semibold text-gray-900" id="title_source">
-                        Update Produk
-                    </h3>
-                    <button type="button" onclick="sourceModalClose()"
-                        class="text-black bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center">
+            <div class="w-full md:w-1/2 relative bg-white rounded-lg shadow mx-5 px-4">
+                <div class="flex items-start justify-between p-4 border-b rounded-t">
+                    <h3 class="text-xl font-semibold text-gray-900" id="title_source">Tambah Transaksi</h3>
+                    <button type="button" onclick="sourceModalClose(this)" data-modal-target="sourceModal"
+                        class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center"
+                        data-modal-hide="defaultModal">
                         <i class="fa-solid fa-xmark"></i>
                     </button>
                 </div>
-                <div class="p-4">
-                    <form method="POST" id="formSourceModalEdit" enctype="multipart/form-data">
-                        @csrf
-                        @method('PUT')
-                        <div class="flex flex-col space-y-6">
-
-                            <!-- Gambar Produk -->
-                            <div class="mb-5">
-                                <label for="image_edit" class="block mb-2 text-sm font-medium text-gray-900">Gambar
-                                    Produk</label>
-                                <input type="file" id="image_edit" name="image_edit"
-                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                                    accept="image/*" />
-                            </div>
-
-                            <!-- Nama Produk -->
-                            <div class="mb-5">
-                                <label for="name_edit" class="block mb-2 text-sm font-medium text-gray-900">Nama
-                                    Produk</label>
-                                <input type="text" id="name_edit" name="name_edit"
-                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                                    required />
-                            </div>
-
-                            <!-- Kategori -->
-                            <div class="mb-5">
-                                <label for="category_id_edit"
-                                    class="block mb-2 text-sm font-medium text-gray-900">Kategori</label>
-                                <select id="category_id_edit" name="category_id_edit"
-                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                                    required>
-                                    <option value="">Pilih Kategori</option>
-                                    @foreach ($kategori as $k)
-                                        <option value="{{ $k->id }}">{{ $k->name }}</option>
-                                    @endforeach
-                                </select>
-                            </div>
-
-                            <!-- Jumlah -->
-                            <div class="mb-5">
-                                <label for="jumlah_edit"
-                                    class="block mb-2 text-sm font-medium text-gray-900">Jumlah</label>
-                                <input type="number" id="jumlah_edit" name="jumlah_edit"
-                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                                    required />
-                            </div>
-
-                            <!-- Kategori -->
-                            <div class="mb-5">
-                                <label for="satuan_edit"
-                                    class="block mb-2 text-sm font-medium text-gray-900">Satuan</label>
-                                <select id="satuan_edit" name="satuan_edit"
-                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                                    required>
-                                    <option value="">Pilih Satuan</option>
-                                    <option value="Pcs">Pcs</option>
-                                    <option value="Pack">Pack</option>
-                                    <option value="Dus">Dus</option>
-                                    <option value="Botol">Botol</option>
-                                    <option value="Cup">Cup</option>
-                                </select>
-                            </div>
-
-                            <!-- Harga Beli -->
-                            <div class="mb-5">
-                                <label for="harga_beli_edit" class="block mb-2 text-sm font-medium text-gray-900">Harga
-                                    Beli</label>
-                                <input type="number" id="harga_beli_edit" name="harga_beli_edit"
-                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                                    required />
-                            </div>
-
-                            <!-- Harga Jual -->
-                            <div class="mb-5">
-                                <label for="harga_jual_edit"
-                                    class="block mb-2 text-sm font-medium text-gray-900">Harga Jual</label>
-                                <input type="number" id="harga_jual_edit" name="harga_jual_edit"
-                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                                    required />
-                            </div>
-
-                            <!-- Stok -->
-                            <div class="mb-5">
-                                <label for="stock_edit"
-                                    class="block mb-2 text-sm font-medium text-gray-900">Stock</label>
-                                <input type="number" id="stock_edit" name="stock_edit"
-                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                                    required />
-                            </div>
-
-                            <!-- Deskripsi -->
-                            <div class="mb-5">
-                                <label for="description_edit"
-                                    class="block mb-2 text-sm font-medium text-gray-900">Deskripsi</label>
-                                <textarea id="description_edit" name="description_edit"
-                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"></textarea>
-                            </div>
-
-                        </div>
-                        <div
-                            class="flex items-center p-4 space-x-2 border-t border-gray-200 rounded-b sticky bottom-0 bg-white z-10">
-                            <button type="submit" id="formSourceButtonEdit"
-                                class="bg-green-400 m-2 w-40 h-10 rounded-xl hover:bg-green-500">Simpan</button>
-                            <button type="button" onclick="sourceModalClose()"
-                                class="bg-red-500 m-2 w-40 h-10 rounded-xl text-white hover:shadow-lg hover:bg-red-600">Batal</button>
-                        </div>
-                    </form>
-                </div>
+                <form method="POST" id="formSourceModal">
+                    @csrf
+                    <div class="mb-4">
+                        <div id="ProdukContainer"></div>
+                        <div class="mt-4 border-t pt-2"></div>
+                        <p class="text-lg font-bold">Total Pembayaran: <span id="totalPayment">Rp. 0</span></p>
+                    </div>
+                    <div class="flex items-center p-4 space-x-2 border-t border-gray-200 rounded-b">
+                        <button type="submit" id="formSourceButton"
+                            class="bg-green-400 m-2 w-40 h-10 rounded-xl hover:bg-green-500">Simpan</button>
+                        <button type="button" data-modal-target="sourceModal" onclick="sourceModalClose(this)"
+                            class="bg-red-500 m-2 w-40 h-10 rounded-xl text-white hover:shadow-lg hover:bg-red-600">Batal</button>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
+
+    <!-- Footer Fixed -->
+    <footer class="fixed bottom-0 left-0 w-full bg-gray-900 text-white py-3 shadow-md" onclick="return functionAdd()">
+        <div class="w-full mx-auto">
+            <div class="max-w-7xl mx-auto flex items-center justify-between px-4">
+                <div class="w-20 max-w-xs">
+                    <label for="total" class="block mb-1 text-sm font-medium text-white">Total</label>
+                </div>
+                <input type="text" id="total" name="total"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="Total" readonly />
+            </div>
+    </footer>
+
     <script>
         const functionAdd = () => {
             const formModal = document.getElementById('formSourceModal');
             const modal = document.getElementById('sourceModal');
 
             // Set form action URL
-            let url = "{{ route('produk.store') }}";
-            document.getElementById('title_source').innerText = "Add Produk";
+            let url = "{{ route('transaksi.store') }}";
+            document.getElementById('title_source').innerText = "Transaksi";
             formModal.setAttribute('action', url);
 
             // Display the modal
@@ -192,58 +114,52 @@
             }
         }
 
-        const editSourceModal = (button) => {
-            const formModal = document.getElementById('formSourceModalEdit');
-            const modalTarget = button.dataset.modalTarget;
-            const id = button.dataset.id;
-            const name = button.dataset.name;
-            const kategori = button.dataset.kategori;
-            const jumlah = button.dataset.jumlah;
-            const satuan = button.dataset.satuan;
-            const harga_beli = button.dataset.harga_beli;
-            const harga_jual = button.dataset.harga_jual;
-            const stock = button.dataset.stock;
-            const description = button.dataset.description;
-            let url = "{{ route('produk.update', ':id') }}".replace(':id', id);
-
-            document.getElementById('formSourceModalEdit').setAttribute('action', url);
-
-            //create for jumlah, satuan, harga_beli, harga_jual, stock
-
-            document.getElementById('name_edit').value = name;
-            document.getElementById('category_id_edit').value = kategori;
-            document.getElementById('jumlah_edit').value = jumlah;
-            document.getElementById('satuan_edit').value = satuan;
-            document.getElementById('harga_beli_edit').value = harga_beli;
-            document.getElementById('harga_jual_edit').value = harga_jual;
-            document.getElementById('stock_edit').value = stock;
-            document.getElementById('description_edit').value = description;
-
-            document.getElementById('sourceModalEdit').classList.remove('hidden');
+        const sourceModalClose = (element) => {
+            const modal = document.getElementById(element.getAttribute('data-modal-target'));
+            modal.classList.remove('flex');
+            modal.classList.add('hidden');
         }
 
-        const sourceModalClose = () => {
-            document.getElementById('sourceModalEdit').classList.add('hidden');
-            document.getElementById('sourceModal').classList.add('hidden');
+        const closeModal = () => {
+            const modal = document.getElementById('modal');
+            modal.classList.add('hidden');
         }
 
-        const kategoriDelete = async (id, name) => {
-            let tanya = confirm(`Apakah anda yakin untuk menghapus produk ${name} ?`);
-            if (tanya) {
-                await axios.post(`/produk/${id}`, {
-                        '_method': 'DELETE',
-                        '_token': $('meta[name="csrf-token"]').attr('content')
-                    })
-                    .then(function(response) {
-                        // Handle success
-                        location.reload();
-                    })
-                    .catch(function(error) {
-                        // Handle error
-                        alert('Error deleting record');
-                        console.log(error);
-                    });
-            }
+        const addCart = (id, name, price) => {
+            const container = document.getElementById('ProdukContainer');
+            const productDiv = document.createElement('div');
+            productDiv.classList.add('flex', 'gap-2', 'items-center', 'border-b', 'py-2');
+            productDiv.innerHTML = `
+                <span class="w-2/6">${name}</span>
+                <input type="number" class="w-1/6 p-2 border rounded" value="1" min="1" oninput="calculateTotal(this, ${price})">
+                <input type="text" class="w-2/6 p-2 border rounded bg-gray-100" value="Rp. ${price}" readonly>
+                <button type="button" onclick="removeProduct(this)" class="w-1/6 bg-red-500 text-white py-1 rounded hover:bg-red-600">Hapus</button>
+            `;
+            container.appendChild(productDiv);
+            updateSubtotal();
+        }
+
+        const removeProduct = (button) => {
+            button.parentElement.remove();
+            updateSubtotal();
+        }
+
+        const calculateTotal = (input, price) => {
+            let quantity = parseInt(input.value) || 1;
+            let totalField = input.nextElementSibling;
+            let total = price * quantity;
+            totalField.value = `Rp. ${total}`;
+            updateSubtotal();
+        }
+
+        const updateSubtotal = () => {
+            let totalFields = document.querySelectorAll('#ProdukContainer input[readonly]');
+            let subtotal = 0;
+            totalFields.forEach(field => {
+                subtotal += parseInt(field.value.replace(/[^0-9]/g, '')) || 0;
+            });
+            document.getElementById('totalPayment').innerText = `Rp. ${subtotal}`;
+            document.getElementById('total').value = `Rp. ${subtotal}`;
         }
     </script>
 </x-app-layout>
